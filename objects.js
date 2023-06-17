9b6741299182d3f1610e4103ae1ac053c49f4d53
Maryam <maryamjaved.xo@gmail.com>
2023-06-18 00:28:19 +0500

    method chaining

--- objects.js
+++ objects.js
@@ -1,13 +1,44 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
+// var userOne = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu',
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout(){
+//         console.log(this.email, 'has logged out');
+//     }
+// };
 
-userOne.login();
-userOne.logout();
+// userOne.login();
+// userOne.logout();
+
+
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+        return this;        //so we could chain method
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;    
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+
+
+//chaining methodg
+userOne.login().updateScore().updateScore().logout();
+
+console.log(userOne);
