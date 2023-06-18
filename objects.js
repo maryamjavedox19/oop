c2cd9f0df03307f389f36326e1c5b0dc130ce1fa
Maryam <maryamjaved.xo@gmail.com>
2023-06-18 11:22:27 +0500

    prototype inheritance

--- objects.js
+++ objects.js
@@ -13,8 +13,9 @@
 // userOne.logout();
 
 
-//Method inside Prototype
 
+
+//prototype inheritance
 function User(email, name){
     this.email = email;
     this.name = name;
@@ -31,8 +32,20 @@ User.prototype.logout = function(){
     console.log(this.email, 'has logged out');
 };
 
+function Admin(...args){
+    User.apply(this, args);
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    });
+};
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
